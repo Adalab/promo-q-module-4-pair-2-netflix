13c38639f51485fabf7ad2016fd4eb3ad810fe98
Marianela Landoni Herrera <mlandonih@hotmail.com>
2022-09-08 11:13:44 +0200

    Add database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const express = require("express");
-const cors = require("cors");
-const movies = require("./data/movies.json");
+const express = require('express');
+const cors = require('cors');
+const movies = require('./data/movies.json');
+const Database = require('better-sqlite3');
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+const db = new Database('./src/db/database.db', {
+  verbose: console.log,
+});
 
 // init express aplication
 const serverPort = 4000;
@@ -13,25 +18,31 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get("/movies", (req, resp) => {
+server.get('/movies', (req, resp) => {
+  const query = db.prepare(`SELECT * FROM movies`);
+  const allMovies = query.all();
+  console.log(allMovies);
   resp.json({
     success: true,
-    movies: movies,
+    movies: allMovies,
   });
 });
 
-server.set("view engine", "ejs");
-server.get("/movie/:movieId", (req, res) => {
+server.set('view engine', 'ejs');
+server.get('/movie/:movieId', (req, res) => {
   console.log(req.params.movieId);
   const foundMovie = movies.find((movie) => {
     return movie.id === req.params.movieId;
   });
   console.log(foundMovie);
-  //res.render("movie", foundMovie);
+  res.render('movie', foundMovie);
 });
 
-const staticServer = "./src/public-react";
+const staticServer = './src/public-react';
 server.use(express.static(staticServer));
 
-const staticPhotos = "./src/public-movies-images";
+const staticPhotos = './src/public-movies-images';
 server.use(express.static(staticPhotos));
+
+const staticStyles = './src/styles';
+server.use(express.static(staticStyles));
